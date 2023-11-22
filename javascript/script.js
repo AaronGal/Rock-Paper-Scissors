e022d5fe9f825d3c7d6d43fb5ba1cdffa42fc8b2
AaronGal <149002397+AaronGal@users.noreply.github.com>
2023-11-22 19:23:26 +0000

    .

--- javascript/script.js
+++ javascript/script.js
@@ -1,28 +1,35 @@
 function game(){
     const array = ["rock","paper","scissors"];
-    var botanswer;
-    botanswer = array[Math.floor(Math.random()*array.length)];
+     
     var input;
-    input = prompt("Rock, paper, scissors?").toLowerCase();
-    if (input === botanswer){
-        document.getElementById("Rock,paper,scissors").innerHTML=("It is a draw, you both selected "+ input)
-    }
-    else if(input === "rock" && (botanswer ==="scissors")){
-        document.getElementById("Rock,paper,scissors").innerHTML="You win, opponent selected scissors"
-    }
-    else if(input === "rock"&& (botanswer === "paper")){
-        document.getElementById("Rock,paper,scissors").innerHTML="You lose, opponent selected paper"
-    }
-    else if(input === "paper"&& (botanswer === "rock")){
-        document.getElementById("Rock,paper,scissors").innerHTML="You win opponent selected rock"
-    }
-    else if(input === "paper"&& (botanswer === "scissors")){
-        document.getElementById("Rock,paper,scissors").innerHTML="You lose opponent selected scissors"
-    }
-    else if(input === "scissors"&& (botanswer === "paper")){
-        document.getElementById("Rock,paper,scissors").innerHTML="You win opponent selected paper"
-    }
-    else if(input === "scissors"&& (botanswer === "rock")){
-        document.getElementById("Rock,paper,scissors").innerHTML="You lose opponent selected rock"
+    
+    for(i=0; i<3; i++){
+        var botanswer = array[Math.floor(Math.random() * array.length)];
+        input = prompt("Rock, paper, scissors?").toLowerCase();
+
+        if (input === botanswer){
+            alert("Game " + (i+1) +": It is a draw, you both selected "+ input)
+        }
+        else if(input === "rock" && botanswer ==="scissors"){
+            alert("Game " + (i+1) +": You win, opponent selected scissors. Congratulations!")
+            break;
+        }
+        else if(input === "rock"&& botanswer === "paper"){
+            alert("Game " + (i+1) +": You lose, opponent selected paper")
+        }
+        else if(input === "paper"&& botanswer === "rock"){
+            alert("Game " + (i+1) +": You win opponent selected rock. Congratulations!")
+            break;
+        }
+        else if(input === "paper"&& botanswer === "scissors"){
+            alert("Game " + (i+1) +": You lose opponent selected scissors")
+        }
+        else if(input === "scissors"&& botanswer === "paper"){
+            alert("Game " + (i+1) +": You win opponent selected paper. Congratulations!")
+            break;
+        }
+        else if(input === "scissors"&& botanswer === "rock"){
+            alert("Game " + (i+1) +": You lose opponent selected rock")
+        }
     }
 }
